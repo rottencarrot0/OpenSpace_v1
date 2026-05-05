0827e1e378a4dbba5b298a057deb25c43b47867c
rottencarrot0 <qpwo3824@gmail.com>
2026-05-05 15:01:07 +0900

    프로젝트 세팅

---
+++ src/main/java/openspace/page/SpringJspMybatisOpenSpaceApplication.java
@@ -0,0 +1,13 @@
+package openspace.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJspMybatisOpenSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJspMybatisOpenSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/openspace/page/SpringJspMybatisOpenSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package openspace.page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJspMybatisOpenSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
