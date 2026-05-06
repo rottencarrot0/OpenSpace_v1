c8db92f61a68884be28627f62e7c3f972834d2af
rottencarrot0 <qpwo3824@gmail.com>
2026-05-06 17:18:58 +0900

    UserController 추가

--- src/main/java/openspace/page/controller/UserController.java
+++ src/main/java/openspace/page/controller/UserController.java
@@ -1,4 +1,34 @@
 package openspace.page.controller;
 
+import jakarta.validation.Valid;
+import openspace.page.dto.user.UserRegister;
+import openspace.page.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/register")
+    public String registerForm() {
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid UserRegister register, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "user/register";
+        }
+        userService.register(register);
+
+        return "ok";
+    }
 }
