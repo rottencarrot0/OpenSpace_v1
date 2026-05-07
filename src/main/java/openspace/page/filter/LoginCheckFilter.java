80a7ae4d5f4947bb767e128df592b609d0fe4a36
rottencarrot0 <qpwo3824@gmail.com>
2026-05-07 20:30:23 +0900

    로그인 체크 필터 수정

--- src/main/java/openspace/page/filter/LoginCheckFilter.java
+++ src/main/java/openspace/page/filter/LoginCheckFilter.java
@@ -14,21 +14,19 @@ import java.io.IOException;
 @Slf4j
 public class LoginCheckFilter implements Filter {
 
-    private static final String[] no_check_pass = {"/", "/user/login", "/user/logout"};
+    private static final String[] no_check_pass = {"/", "/user/login", "/user/logout", "/user/register"};
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String requestURI = httpRequest.getRequestURI();
-        
-        // 현재 등록되어 있는 url 들과 매칭이 되는지 확인하기
-
 
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
         try {
             if(isNoCheckPass(requestURI)) {
                 // 인증 로직 시작
+                log.info("인증 체크 필터 시작 {}", requestURI);
                 HttpSession session = httpRequest.getSession(false);
                 if(session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
                     log.info("미인증 사용자 요청 {}", requestURI);
