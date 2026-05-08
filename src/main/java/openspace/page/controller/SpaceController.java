c2bde8bbf92f1b3901ee4663dd68f54e599dd7fd
rottencarrot0 <qpwo3824@gmail.com>
2026-05-08 11:20:19 +0900

    space 추가 진행중

--- src/main/java/openspace/page/controller/SpaceController.java
+++ src/main/java/openspace/page/controller/SpaceController.java
@@ -3,36 +3,51 @@ package openspace.page.controller;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import openspace.page.domain.SessionConst;
+import openspace.page.domain.User;
 import openspace.page.dto.space.SpaceRegister;
+import openspace.page.service.SpaceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/space")
 public class SpaceController {
 
+    @Autowired
+    private SpaceService spaceService;
+
     @GetMapping("/register")
     public String newSpace() {
         return "space/form";
     }
+
 
     // 등록 / 수정 시 같은 controller 사용한다.
     @PostMapping
     public String createSpace(
             @Valid SpaceRegister register,
             BindingResult bindingResult,
+            @RequestParam(required = false) List<MultipartFile> images,
             HttpSession session,
             Model model
-    ) {
+    ) throws IOException {
         if(bindingResult.hasErrors()) {
             model.addAttribute("errorMessage", bindingResult.getAllErrors().get(0).getDefaultMessage());
             return "space/form";
         }
+        User loginUser = (User)session.getAttribute(SessionConst.LOGIN_USER);
+
+        spaceService.createSpace(register, images, loginUser.getId());
         // PRG (post redirect get)
         return "redirect:/space/my_space";
     }
