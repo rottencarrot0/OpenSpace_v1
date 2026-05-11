2789f2a3ef90f5d28abd37e3919c1eb9e732cd9e
rottencarrot0 <qpwo3824@gmail.com>
2026-05-11 13:12:03 +0900

    비즈니스 예외 추가

---
+++ src/main/java/openspace/page/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package openspace.page.exception;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
